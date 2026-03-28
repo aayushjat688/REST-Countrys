5a1954af908c7666c421999c6a3c0c2b4971b008
aayushjat688 <aayushjat688@gmail.com>
2026-03-28 15:11:50 +0530

    added name flag population etc with help of api in js

---
+++ script.js
@@ -0,0 +1,32 @@
+let html = '';
+let countryContainer = document.querySelector('.countries-container')
+fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital')
+.then((res)=>{
+    return res.json();
+})
+.then((data)=>{
+    data.forEach((country)=>{
+        const flag = country.flags.svg;
+        const name = country.name.official;
+        const population = country.population;
+        const region = country.region;
+        const capital = country.capital;
+
+         html += `
+        <a href="#"><div class="country-card">
+                <img src="${flag}" alt="flag">
+                 <div class="country-card-text-item">
+                    <h3>${name}</h3>
+               
+                 <p><strong>Population:</strong>${population}</p>
+                    <p><strong>Region:</strong>${region}</p>
+                    <p><strong>Capital:</strong>${capital}</p>
+                 </div>
+                 </div>
+            </a>
+        `
+        
+    })
+    countryContainer.innerHTML = html;
+})
+
