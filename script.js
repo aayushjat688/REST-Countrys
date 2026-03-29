1cde485d013ccd49635b2f082d9582a8d0d72758
aayushjat688 <aayushjat688@gmail.com>
2026-03-29 10:02:57 +0530

    half page done

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let html = '';
 let countryContainer = document.querySelector('.countries-container')
+const countryCard = document.querySelector('.country-card')
 fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital')
 .then((res)=>{
     return res.json();
@@ -11,9 +12,14 @@ fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,ca
         const population = country.population;
         const region = country.region;
         const capital = country.capital;
+        
+        
+        
 
          html += `
-        <a href="#"><div class="country-card">
+        <a href="
+        /country.html?
+        "><div class="country-card">
                 <img src="${flag}" alt="flag">
                  <div class="country-card-text-item">
                     <h3>${name}</h3>
