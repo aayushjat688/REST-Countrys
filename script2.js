4dcdbf443e3bd553d172d3f79f2f6db18656db05
aayushjat688 <aayushjat688@gmail.com>
2026-03-30 12:02:59 +0530

    make responsive

--- script2.js
+++ script2.js
@@ -1,4 +1,6 @@
 let html = '';
+let html2 = '';
+
 const urlParams = new URLSearchParams(location.search);
 const countryName = urlParams.get('name');
 
@@ -7,35 +9,38 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
     return response.json();
 })
 .then((data)=>{
-    console.log(data);
+    // console.log(data);
+    // console.log(Object.values(data[0].currencies));
     const country = data[0];
-    const cName = country.name.official
-    const population = country.population;
-    
+    // console.log(country.borders[0]);
+
+
     html = `
      <main>
         <img src="${country.flags.svg}" alt="">
          <div class="detail-main">
-            <h3>${cName}</h3>
+            <h3>${country.name.official}</h3>
             <div class="detail">
                <div class="left-detail">
-                 <p class="native-name"><strong>Native Name:</strong>${cName}</p>
-                <p><strong>Population:</strong>${population}</p>
+                 <p class="native-name"><strong>Native Name:</strong>${country.name.official}</p>
+                <p><strong>Population:</strong>${country.population}</p>
                 <p><strong>Region:</strong>${country.region}</p>
                 <p><strong>Sub Region:</strong>${country.subregion}</p>
                 <p><strong>Capital:</strong>${country.capital}</p>
                </div>
                 <div class="right-detail">
                     <p><strong>Top Level Domain:</strong>${country.tld[0]}</p>
-                    <p><strong>Currencies:</strong>${country.currencies[0]}</p>
-                    <p><strong>Languages:</strong>${country.languages[0]}</p>
+                    <p><strong>Currencies:</strong>${Object.values(country.currencies).map(currency => currency.name).join(', ')}</p>
+                    <p><strong>Languages:</strong>${Object.values(country.languages).join(', ')}</p>
                 </div>
             </div>
             
          </div>
-    </main>
+    </main>   
     `
+    
     document.querySelector('.detail-container').innerHTML = html;
+    document.querySelector('.border-countries').innerHTML = html2;
 })
 
 
